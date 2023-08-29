196339b9f31a8fa102282ca822ce4503aa494519
briellekwarta19 <brielle.kwarta19@houghton.edu>
2023-08-28 17:33:46 -0700

    updating mod

--- scripts/Multistate/effort1hr_hocc_datD.R
+++ scripts/Multistate/effort1hr_hocc_datD.R
@@ -35,15 +35,20 @@ model{
 
 #### PRIORS ####
   #Erradication:
-  eps.l ~ dbeta(eps.la,eps.l.b) #erradication when at low state
-  eps.h ~ dbeta(eps.h.a,eps.h.b) #erradication when at high state
+  eps.l0 ~ dbeta(eps.la,eps.l.b) #erradication when at low state
+  eps.l1 ~ dnorm(eps.l1.mean, eps.l1.tau) #effect of effort 
+  eps.l1.tau <- 1/(eps.l1.sd * eps.l1.sd)
+  
+  eps.h0 ~ dbeta(eps.h.a,eps.h.b) #erradication when at high state
+  eps.h1 ~ dnorm(eps.h1.mean, eps.h1.tau) #effect of effort 
+  eps.h1.tau <- 1/(eps.h1.sd * eps.h1.sd)
   
   #Invasion:
   gamma.0 ~dnorm(gamma.0.mean,gamma.0.tau) #intrinsic invasion probability
   gamma.0.tau <- 1/(gamma.0.sd*gamma.0.sd)
   gamma.1 ~dnorm(gamma.1.mean, gamma.1.tau) #effect of site characteristics on invasion probability
   gamma.1.tau <- 1/(gamma.1.sd*gamma.1.sd)
-  gamma.2 ~dnorm(gamma.2.mean, gamma.2.tau) #effect of site characteristics on invasion probability
+  gamma.2 ~dnorm(gamma.2.mean, gamma.2.tau) #effect of site Neighboring invasion state
   gamma.2.tau <- 1/(gamma.2.sd*gamma.2.sd)
   
   #State transition:
@@ -59,20 +64,17 @@ model{
   p.h0 ~ dbeta(p.h0.a, p.h0.b) #base detection for high state
   p.h1 ~ dnorm(p.h1.mean, p.h1.tau) #effect of effort 
   p.h1.tau <- 1/(p.h1.sd * p.h1.sd)
-  
-  for(i in 1:n.sites){
-    for(j in 1:n.obs){
-      logit(pD.l[i,j]) <- p.l0 + p.l1*logeffort[i,j]
-      logit(pD.h[i,j]) <- p.h0 + p.h1*logeffort[i,j]
-    }
-  }
-  
 
 #--------------------------------------------------#
 # STATE TRANSITION
 for (i in 1:n.sites){  
   # State transition probabilities: probability of S(t+1) given S(t)
   for (t in 1:n.weeks){
+  
+    logit(gamma[i,t]) <-gamma0 + gamma1*site.char[i] + gamma2*D[i,t] #invasion probability
+    logit(eps.l[i,t]) <- eps.l0 + eps.l1*rem.vec[i,t]
+    logit(eps.h[i,t]) <- eps.h0 + eps.h1*rem.vec[i,t]
+    
     #index = [current state, location, time, future state]
     #empty stay empty
     ps[1,i,t,1] <- 1-gamma[i,t] #1-gamma = not invasion probability
@@ -84,212 +86,289 @@ for (i in 1:n.sites){
     ps[1,i,t,3] <- 0 #invasion probability
 
     #low abundance to empty
-    ps[2,i,t,1] <- (eps.l*rem.vec[i]) #erradication probability
+    ps[2,i,t,1] <- eps.l[i,t] #erradication probability
                                       # rem.vec[i] = 0,1 if 0, then no removal and no erradiction
     
     #low abundance to low abundance
-    ps[2,i,t,2] <- (1- eps.l*rem.vec[i])*(1-phi.lh) #erradication failure probability
+    ps[2,i,t,2] <- (1- eps.l[i,t])*(1-phi.lh) #erradication failure probability
     
     #low abundance to high abundance
-    ps[2,i,t,3] <- (1- eps.l*rem.vec[i])*(phi.lh)
+    ps[2,i,t,3] <- (1- eps.l[i,t])*(phi.lh)
     
     #high abundance to empty
-    ps[3,i,t,1] <- (eps.h*rem.vec[i]) #erradication probability
+    ps[3,i,t,1] <- eps.h[i,t] #erradication probability
     
     #high abundance to low abundance
-    ps[3,i,t,2] <- (1- eps.h*rem.vec[i])*(1-phi.hh) #erradication failure probability
+    ps[3,i,t,2] <- (1- eps.h[i,t])*(1-phi.hh) #erradication failure probability
     
     #low abundance to high abundance
-    ps[3,i,t,3] <- (1- eps.h*rem.vec[i])*(phi.hh)
+    ps[3,i,t,3] <- (1- eps.h[i,t])*(phi.hh)
     
-    logit(gamma[i,t]) <-gamma0 + gamma1*site.char[i] + gamma2*D[i,t] +  #invasion probability
     
     #--------------------------------------------------#
     # OBSERVATION PROBABILITIES 1 (for detection/nondetection data)
     
     for(j in 1:n.obs){
 
+      logit(pD.l[i,j]) <- p.l0 + p.l1*logeffort[i,j]
+      logit(pD.h[i,j]) <- p.h0 + p.h1*logeffort[i,j]
+
       #Empty and not observed  
-      po_datD[1,i,j,t,1] <- 1
+      po.datD[1,i,j,t,1] <- 1
       
       #Empty and observed
-      po_datD[1,i,j,t,2] <- 0
+      po.datD[1,i,j,t,2] <- 0
    
       #Low state and not observed
-      po_datD[2,i,j,t,1] <- 1-pD.l[i,j] #not detected probability
+      po.datD[2,i,j,t,1] <- 1-pD.l[i,j] #not detected probability
       
       #Low state and observed
-      po_datD[2,i,j,t,2] <- pD.l[i,j] #detection probability
+      po.datD[2,i,j,t,2] <- pD.l[i,j] #detection probability
       
       #High state and not observed
-      po_datD[3,i,j,t,1] <- 1-pD.h[i,j] #not detected probability
+      po.datD[3,i,j,t,1] <- 1-pD.h[i,j] #not detected probability
       
       #High state and observed
-      po_datD[3,i,j,t,2] <- pD.h[i,j] #detection probability
+      po.datD[3,i,j,t,2] <- pD.h[i,j] #detection probability
       
     } #j 
   } #t
 } #i
 
-#### START FIXING HERE ####
   #### Likelihood ####
-  for (i in 2:(n.sites-1)){
+  for (i in 1:n.sites){
     # Define state at beginning
-    State[i,1] <- S.init[i] #we know state at the start
-    D[i,1] <- D.init[i]
+    State[i,1] <- S.init[i] #we know state at the start?
+    D[i,1] <- D.init[i]     #we know neighbor states at the start
     
-    for (t in 2:n.weeks){ #12
+    for (t in 2:n.weeks){ 
       # State process: state given previous state and transition probability
       State[i,t] ~ dcat(ps[State[i,t-1], i, t-1, ]) 
       
-      D[i,t] <- State[i-1,t] + State[i+1,t] #state of neighbors 
-      
+      D[i,t] <- sum(State[neighbors[i,], t])/2 #state of neighbors 
      
     } #t
-      
-      #Derived parameter:
-      
-      State.fin[i] <- State[i,n.months] #state after 12 months
-  } #i
-  
-  #Fill in above for edge sites (1 and n.sites)
-  State[1,1] <- S.init[1] 
-  State[n.sites,1] <- S.init[n.sites] 
-  D[1,1] <- D.init[1]
-  D[n.sites,1] <- D.init[n.sites]
-  
-   for (t in 2:n.months){
-      State[1,t] ~ dcat(ps[State[1,t-1], 1, t-1, ])
-      State[n.sites,t] ~ dcat(ps[State[n.sites,t-1], n.sites, t-1, ])
-      D[1,t] <- State[2,t]
-      D[n.sites,t] <- State[n.sites-1,t]
-    } 
 
-  State.fin[1] <- State[1,n.months]
-  State.fin[n.sites] <- State[n.sites,n.months]
-  
-  
-  for(h in 1:n.obs){
-  
-    # Observation process: draw observation given state
-    y1[site.obsD[h]] ~ dcat(po_datD[State.fin[site.obsD[h]], site.obsD[h], n.months-1, ])
+    for(j in 1:n.obs){
+      for(t in 1:n.weeks){
+        # Observation process: draw observation given state
+        yD[i,j,t] ~ dcat(po.datD[State[i,t], i, j, t-1,])
+      } #t
+    } #j
     
+    #Derived parameter:
+    State.fin[i] <- State[i,n.weeks] #state after 4 weeks
     
-  } #obs sites
-  
+  } #i
+
 
 } #end model
 ", fill = TRUE)
 sink()
 
 #### Path Name ####
-path <- here::here("results", "ESA", "e5_hocc")
-res <- c('results/ESA/e5_hocc') 
+path <- here::here("results", "Multistate", "e1hr_hocc_datD")
+res <- c('results/Multistate/e1hr_hocc_datD') 
 
 #### Data ####
-n.sims <-  25 #100
-n.sites <- 50 #number of sites
-n.years <- 10 
+n.sims <-  2 #25 #100
+n.sites <- 40 #number of sites
+n.years <- 10
+n.weeks <- 4
 n.occs <- 2 #number of occasions for occupancy data collection
+n.states <- 3
 
-psi0 <- 0.5 #base occupancy
-psi1 <- -30 #effect of previous removal
-psi2 <- 0.5  #effect of site characteristics 
-psi3 <-2 #effect of previous year state
-a0 <- 0.9 #base detection
-a1 <- 1 #effect of effort
-n.resource <- 70 #total resources we can use
+eps.l0 <- 0.3 #base eradication at low state
+eps.l1 <- 5 #effect of eradication at low state
+eps.h0 <- 0.2 #base eradication at high state
+eps.h1 <- 2 #effect of eradication at high state
+gamma.0 <- 0.3 #intrinsic invasion probability
+gamma.1 <- 0.1 #effect of site characteristics
+gamma.2 <- 1 #effect of neighboring invasion state
+phi.lh <- 0.1 #transition from low to high
+phi.hh <- 0.8 #transition from high to high
 
-##### true state year 1 ####
-psi <- State <- array(NA,c(n.sites, n.years+1, n.sims)) 
+p.l0 <- 0.3 #base detection for low state
+p.l1 <- 1 #effect of effort
+
+p.h0 <- 0.8 #base detection for high state
+p.h1 <- 1 #effect of effort
+
+n.resource <- 20 #total resources we can use each week
+
+#neighbor data
+num.neighbors <- 2 #one upstream, one downstream
+neighbors <- matrix(NA, nrow = n.sites, ncol = num.neighbors) #neighbors matrix, each row (site) identifies the neighbors for that site 
+neighbors[1,1] <- 2
+neighbors[2:n.sites, 1] <- seq(1,n.sites-1)
+neighbors[n.sites,2] <- n.sites-1
+neighbors[1:(n.sites-1), 2] <- seq(2,n.sites)
+
 # effect of habitat quality on occupancy
-set.seed(03222021)
-#site.char <- runif(n.sites) #might be multivariate, this is data
-
+#set.seed(03222021)
+#site.char <- runif(n.sites) 
 path2 <- here::here("data")
-file_name = paste(path2, 'Site_char.csv',sep = '/')
+file_name = paste(path2, 'Site_char_multi.csv',sep = '/')
 # write.csv(site.char,file_name)
-
 site.chardat <- read.csv(file_name)
-
 site.char <- site.chardat[,2]
 
-# set.seed(03222021)
+##### week 1 -state ####
+#set.seed(03222021)
+# State.init <- rep(NA, n.sites)
 # 
 # for(i in 1:n.sites){
-#   #psi[i,1,1:n.sims] <- 1/(1+exp(-(psi0 + psi2*site.char[i]))) #inv.logit
-#   psi[i,1,1:n.sims] <- invlogit(psi0 + psi2*site.char[i])
-#   State[i,1,1:n.sims] <- rbinom(1,1,psi[i,1,1:n.sims]) #true state
+#   State.init[i] <- rbinom(1,1,invlogit(gamma.0 + gamma.1*site.char[i])) #true state
 # }
+#write.csv(State.init,file_name)
 
 path2 <- here::here("data")
-file_name = paste(path2, 'States_init.csv',sep = '/')
-
+file_name = paste(path2, 'States_init_multi.csv',sep = '/')
 State.init <- read.csv(file_name)
+State <- array(NA,c(n.sites, n.weeks, n.years+1, n.sims)) #state array
+State[,1,1,1:n.sims] <- State.init[,2] + 1 #initial state
 
-State[,1,1:n.sims] <- State.init[,2]
+D <- array(NA, c(n.sites, n.weeks, n.years+1, n.sims)) #neighbors array
+for(i in 1:n.sites){
+  for(s in 1:n.sims){
+    D[i,1,1,s] <- sum(State[neighbors[i,], 1,1,s])/2 #state of neighbors
+}}
+ 
+ps<- array(NA, c(n.states,n.sites,n.weeks, n.years + 1,n.sims, n.states)) #state probability
+gamma <- array(NA, c(n.sites, n.weeks, n.years+1, n.sims)) #invasion probability
+eps.l <- array(NA, c(n.sites, n.weeks, n.years+1,n.sims)) #eradication at low state
+eps.h <- array(NA, c(n.sites, n.weeks, n.years+1, n.sims)) #eradication at high state
 
-##### occupancy data year 1 ####
+gamma[,1,1,] <-invlogit(gamma.0 + gamma.1*site.char + gamma.2*D[,1,1,]) #invasion (week 1 year 1)
+eps.l[,1,1,] <- invlogit(eps.l0) #eradication low (week 1 year 1)
+eps.h[,1,1,] <- invlogit(eps.h0) #eradication high (week 1 year 1)
+
+ps[1,1:n.sites,1,1,,1] <- 1-gamma[,1,1,] #empty to empty (week 1 year 1)
+ps[1,1:n.sites,1,1,,2] <- gamma[,1,1,] #empty to low (week 1 year 1)
+ps[1,1:n.sites,1,1,,3] <- 0 #empty to high (week 1 year 1)
+
+ps[2,1:n.sites,1,1,,1] <- eps.l[,1,1,] #low to empty (week 1 year 1)
+ps[2,1:n.sites,1,1,,2] <- (1- eps.l[,1,1,])*(1-phi.lh) #low to low (week 1 year 1)
+ps[2,1:n.sites,1,1,,3] <- (1- eps.l[,1,1,])*(phi.lh) #low to high (week 1 year 1)
+
+ps[3,1:n.sites,1,1,,1] <- eps.h[,1,1,] #high to empty (week 1 year 1)
+ps[3,1:n.sites,1,1,,2] <- (1- eps.h[,1,1,])*(1-phi.hh) #high to low (week 1 year 1)
+ps[3,1:n.sites,1,1,,3] <- (1- eps.h[,1,1,])*(phi.hh) #high to high (week 1 year 1)
+
+##### week 1 -occ ####
 #Randomly order all sites (next year order of sites will be based on some factor)
-sites.rem <- array(NA, dim = c(n.sites, n.years, n.sims))
+sites.rem <- array(NA, dim = c(n.sites, n.weeks, n.years, n.sims))
 
-sites.rem[,1,] <- sample(seq(1,n.sites), n.sites, replace = F)
+for(s in 1:n.sims){
+  sites.rem[,1,1,s] <- sample(seq(1,n.sites), n.sites, replace = F)
+}
 
-logsearch.effort <- array(NA, c(n.occs,n.years, n.sims))
+###### effort ####
+#effort is fixed
+search.hours <- 1.1
+removal.hours <- 2
+logsearch.effort <- log(search.hours)
+resource.total <- array(0, c(n.weeks, n.years,n.sims))
 
-#effort is fixed year 1
-logsearch.effort[,1,] <- log(0.5)
-removal.effort <- 2
-resource.total <- array(0, c(n.years,n.sims))
+yD <- array(NA,c(n.sites, n.occs, n.weeks, n.years, n.sims)) #detection/non-detection data
+rem.vec <- array(NA, c(n.sites, n.weeks, n.years, n.sims)) #removal sites array
+po.datD <- array(NA, c(n.states, 2)) #detection probability
 
-obs.pr <- y.obs <- array(NA,c(n.sites, n.occs, n.years, n.sims))
-rem.vec <- array(NA, c(n.sites, n.years, n.sims)) #removal sites array
+pD.l <- invlogit(p.l0 + p.l1*logsearch.effort)
+pD.h <- invlogit(p.h0 + p.h1*logsearch.effort)
+
+po.datD[1,1] <- 1
+po.datD[1,2] <- 0
+
+po.datD[2,1] <- 1-pD.l #not detected probability
+po.datD[2,2] <- pD.l #detection probability
+
+po.datD[3,1] <- 1-pD.h #not detected probability
+po.datD[3,2] <- pD.h #detection probability
 
 #simulate occupancy data for year 1 across sims
 year <- 1
 
+#### CHECK HERE ####
 for(s in 1:n.sims){
-  for(i in sites.rem[,year,s]){
-    
-    #while we still have resources to spend:
-    if(resource.total[year,s] < n.resource){
-      for(j in 1:2){ #detection probability
-        obs.pr[i,j, year, s] <- 1/(1+exp(-(a0 + a1*logsearch.effort[j,year,s])))
+  for(week in 1:n.weeks){
+    if(week > 1){
+      
+      n.pre.visit <- length(which(rem.vec[,week-1,year,s] >= 0)) #number of sites that were sampled last week
+      #put last weeks sampling sites at the end of the sampling queue 
+      sites.rem[,week,year,s] <- c(sites.rem[,(week-1),year,s][-1:-n.pre.visit],
+                                   sites.rem[,(week-1),year,s][1:n.pre.visit])
+      
+      prev.rem.vec2 <- replace(rem.vec[,week-1,year,s], is.na(rem.vec[,week-1,year,s]), 0)
+      
+      for(i in 1:n.sites){
+        State[i,week,year,s] <- rcat(1,ps[State[i,week-1,year,s], i, week-1, year, s,]) 
       }
       
-      #first occasion occupancy data
-      y.obs[i,1, year, s] <- rbinom(1,1,obs.pr[i,1, year, s])*State[i, year, s]
-        
-      #second occasion occupancy data
-      if(y.obs[i,1, year, s]==1){ #if already seen, do not search again and remove
-        y.obs[i,2, year, s] <- NA 
-        rem.vec[i,year,s] <- 1
-        resource.total[year,s] <- resource.total[year,s] + 0.5 + 2
-          
-      }else{
-        y.obs[i,2, year, s] <- rbinom(1,1,obs.pr[i,2, year, s])*State[i, year, s] #occupancy data
-          
-        if(y.obs[i,2, year, s]==1){
-           rem.vec[i,year,s] <- 1
-           resource.total[year,s] <- resource.total[year,s] + 2*0.5 + 2
-        } 
-          
-         #if we don't detect anything, then 
-         if(y.obs[i,2, year, s]==0){
-           rem.vec[i,year,s] <- 0
-           resource.total[year,s] <- resource.total[year,s] + 2*0.5
-         } 
+      for(i in 1:n.sites){
+        D[i,week,year,s] <- sum(State[neighbors[i,], week,year,s])/2 #state of neighbors
       }
-    #if we do not have any more resources to spend:
-    }else{
-      #if we do not have enough data
-      y.obs[i,1:2, year, s] <- NA
-      rem.vec[i,year,s] <- NA
+      
+      
+      #create transition matrix that will be used for next week
+      gamma[,week,year,s] <-invlogit(gamma.0 + gamma.1*site.char + gamma.2*D[,week,year,s]) #week or week-1?
+      eps.l[,week,year,s] <- invlogit(eps.l0 + eps.l1*prev.rem.vec2)
+      eps.h[,week,year,s] <- invlogit(eps.h0 + eps.h1*prev.rem.vec2)
+      
+      ps[1,1:n.sites,week,year,s,1] <- 1-gamma[,week,year,s]
+      ps[1,1:n.sites,week,year,s,2] <- gamma[,week,year,s]
+      ps[1,1:n.sites,week,year,s,3] <- 0 
+      
+      ps[2,1:n.sites,week,year,s,1] <- eps.l[,week,year,s]
+      ps[2,1:n.sites,week,year,s,2] <- (1- eps.l[,week,year,s])*(1-phi.lh) #eradication failure probability
+      ps[2,1:n.sites,week,year,s,3] <- (1- eps.l[,week,year,s])*(phi.lh)
+      
+      ps[3,1:n.sites,week,year,s,1] <- eps.h[,week,year,s] #eradication probability
+      ps[3,1:n.sites,week,year,s,2] <- (1- eps.h[,week,year,s])*(1-phi.hh) #eradication failure probability
+      ps[3,1:n.sites,week,year,s,3] <- (1- eps.h[,week,year,s])*(phi.hh)
+      
     }
-    
-  } #ends site loop
- 
+    for(i in sites.rem[,week,year,s]){
+      #while we still have resources to spend:
+      if(resource.total[week,year,s] < n.resource){
+        
+        #first occasion occupancy data
+        yD[i,1,week, year, s] <- rcat(1, po.datD[State[i,week,year,s], ])
+        
+          
+        #second occasion occupancy data
+        if(yD[i,1,week, year, s]==2){ #if already seen (2), do not search again and remove
+          yD[i,2, week,year, s] <- NA 
+          rem.vec[i,week,year,s] <- 1
+          resource.total[week,year,s] <- resource.total[week,year,s] + search.hours + removal.hours
+            
+        }else{
+          yD[i,2, week, year, s] <- rcat(1, po.datD[State[i,week,year,s], ])
+            
+          if(yD[i,2, week, year, s]==2){ #if seen (2) the second time
+             rem.vec[i,week,year,s] <- 1
+             resource.total[week,year,s] <- resource.total[week,year,s] + 2*search.hours + removal.hours
+          } 
+            
+           #if we don't detect anything
+           if(yD[i,2, week, year, s]==1){
+             rem.vec[i,week,year,s] <- 0
+             resource.total[week,year,s] <- resource.total[week,year,s] + 2*search.hours
+           } 
+        }
+      #if we do not have any more resources to spend:
+      }else{
+        #if we do not have enough data
+        yD[i,1:2, week, year, s] <- NA
+        rem.vec[i,week,year,s] <- NA
+      }
+      
+    } #ends site loop
+  } #ends week loop
 } #ends sims loop
+
+#
+State[,4,year,]
 
 #check
 # y.obs[,,year,2]
