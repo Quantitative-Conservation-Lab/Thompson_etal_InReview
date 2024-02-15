528ab650a0e1f877df78d47435b13bb1f3093d49
briellekwarta19 <brielle.kwarta@gmail.com>
2024-02-14 16:37:21 -0800

    exploring

--- scripts/exploring_parameters.R
+++ scripts/exploring_parameters.R
@@ -11,7 +11,7 @@ search.hours <- seq(0,3)
 neighbors <- seq(0,4)
 n.rem <- length(rem.hours)
 n.search <- length(search.hours)
-n.neighbors
+n.neighbors <- length(neighbors)
 #### eps.l ####
 B0.eps.ls <- seq(-1,2, by = 0.25) #potential B0s
 B1.eps.ls <- seq(0,2, by = 0.25) #potential B1s (min = 0 because want to show removal has a greater effect than no removal)
@@ -92,6 +92,36 @@ ggplot(phih.df, aes(B0,B1, fill= phi.h)) +
   scale_fill_distiller(palette = "Spectral") +
   facet_wrap(~ removal_hours, labeller = label_both)+ labs(title = "phi.h = invlogit(B0 - B1*removal_hours)")
 
+#### p.l ####
+#probability of detecting low
+B0.p.ls <- seq(-2,1, by = 0.25) #potential B0s
+B1.p.ls <- seq(0,2, by = 0.25) #potential B1s
+
+pl.combine <- expand.grid(B0 = B0.p.ls,B1 = B1.p.ls ) #combinations of B0s and B1s
+n.pl <- length(pl.combine$B0)
+pl.array <- array(NA, c(n.pl, n.search))
+
+for(p in 1:n.pl){
+  for(r in 1:n.search)
+    if(search.hours[r] == 0){
+      pl.array[p,r] <- invlogit(pl.combine$B0[p])
+    }else{
+      pl.array[p,r] <- invlogit(pl.combine$B0[p] + log(search.hours[r])*pl.combine$B1[p])
+    }
+  
+}
+
+pl.df <- adply(pl.array, c(1,2))
+colnames(pl.df) <- c("p", "search_hours", "p.h")
+pl.df$search_hours <- as.numeric(pl.df$search_hours) - 1
+pl.df$B0 <- rep(pl.combine$B0, n.search)
+pl.df$B1 <- rep(pl.combine$B1, n.search)
+
+ggplot(pl.df, aes(B0,B1, fill= p.h)) + 
+  geom_tile() +
+  scale_fill_distiller(palette = "Spectral") +
+  facet_wrap(~ search_hours, labeller = label_both)+ labs(title = "p.l = invlogit(B0 + B1*log(search_hours)")
+
 #### p.h ####
 #probability of detecting
 B0.p.hs <- seq(-1,2, by = 0.25) #potential B0s
@@ -99,29 +129,193 @@ B1.p.hs <- seq(0,2, by = 0.25) #potential B1s
 
 ph.combine <- expand.grid(B0 = B0.p.hs,B1 = B1.p.hs ) #combinations of B0s and B1s
 n.ph <- length(ph.combine$B0)
-ph.array <- array(NA, c(n.ph, n.rem))
+ph.array <- array(NA, c(n.ph, n.search))
 
 for(p in 1:n.ph){
   for(r in 1:n.search)
-    #But if search = 0, dont log...
-    ph.array[p,r] <- invlogit(ph.combine$B0[p] + log(search.hours[r])*ph.combine$B1[p])
+    if(search.hours[r] == 0){
+      ph.array[p,r] <- invlogit(ph.combine$B0[p])
+    }else{
+      ph.array[p,r] <- invlogit(ph.combine$B0[p] + log(search.hours[r])*ph.combine$B1[p])
+    }
 }
 
 ph.df <- adply(ph.array, c(1,2))
-colnames(ph.df) <- c("p", "log_search_hours", "p.h")
-ph.df$log_search_hours <- as.numeric(ph.df$removal_hours) - 1
-ph.df$B0 <- rep(ph.combine$B0, n.rem)
-ph.df$B1 <- rep(ph.combine$B1, n.rem)
+colnames(ph.df) <- c("p", "search_hours", "p.h")
+ph.df$search_hours <- as.numeric(ph.df$search_hours) - 1
+ph.df$B0 <- rep(ph.combine$B0, n.search)
+ph.df$B1 <- rep(ph.combine$B1, n.search)
 
 ggplot(ph.df, aes(B0,B1, fill= p.h)) + 
   geom_tile() +
   scale_fill_distiller(palette = "Spectral") +
-  facet_wrap(~ removal_hours, labeller = label_both)+ labs(title = "p.h = invlogit(B0 + B1*log_search_hours)")
+  facet_wrap(~ search_hours, labeller = label_both)+ labs(title = "p.h = invlogit(B0 + B1*log(search_hours)")
 
-#### p.l ####
-#probability of detecting low
-B0.p.ls <- seq(-2,1, by = 0.25) #potential B0s
-B1.p.ls <- seq(0,2, by = 0.25) #potential B1s
-
-#### gamma ####
+#### gamma -min site.char ####
 #invasion probability
+load("parameters.RData")
+site.min <- min(site.char)
+
+B0.gammas <- seq(-2,2, by = 0.25) 
+B1.gammas <- seq(-2,2, by = 0.25) 
+B2.gammas <- seq(-2,2, by = 0.25)
+
+gamma.combine <- expand.grid(B0 = B0.gammas,B1 = B1.gammas,B2 = B2.gammas) #combinations of B0s and B1s
+n.gamma <- length(gamma.combine$B0)
+gamma.array <- array(NA, c(n.gamma, n.neighbors))
+
+for(p in 1:n.ph){
+  for(n in 1:n.search){
+    gamma.array[p,n] <- invlogit(gamma.combine$B0[p] + gamma.combine$B1[p]*site.min + gamma.combine$B2[p]*neighbors[n])
+  }
+}
+
+gamma.df <- adply(ph.array, c(1,2))
+colnames(gamma.df) <- c("p", "neighbor", "gamma")
+gamma.df$neighbor <- as.numeric(gamma.df$neighbor) - 1
+
+#### gamma -mean site.char ####
+#invasion probability
+load("parameters.RData")
+site.mean <- mean(site.char)
+
+B0.gammas <- seq(-2,2, by = 0.25) 
+B1.gammas <- seq(-2,2, by = 0.25) 
+B2.gammas <- seq(-2,2, by = 0.25)
+
+gamma.combine <- expand.grid(B0 = B0.gammas,B1 = B1.gammas,B2 = B2.gammas) #combinations of B0s and B1s
+n.gamma <- length(gamma.combine$B0)
+gamma.array <- array(NA, c(n.gamma, n.neighbors))
+
+for(p in 1:n.ph){
+  for(n in 1:n.search){
+    gamma.array[p,n] <- invlogit(gamma.combine$B0[p] + gamma.combine$B1[p]*site.mean + gamma.combine$B2[p]*neighbors[n])
+  }
+}
+
+gamma.df <- adply(ph.array, c(1,2))
+colnames(gamma.df) <- c("p", "neighbor", "gamma")
+gamma.df$neighbor <- as.numeric(gamma.df$neighbor) - 1
+
+#### gamma -mean site.char ####
+#invasion probability
+load("parameters.RData")
+site.max <- max(site.char)
+
+B0.gammas <- seq(-2,2, by = 0.25) 
+B1.gammas <- seq(-2,2, by = 0.25) 
+B2.gammas <- seq(-2,2, by = 0.25)
+
+gamma.combine <- expand.grid(B0 = B0.gammas,B1 = B1.gammas,B2 = B2.gammas) #combinations of B0s and B1s
+n.gamma <- length(gamma.combine$B0)
+gamma.array <- array(NA, c(n.gamma, n.neighbors))
+
+for(p in 1:n.ph){
+  for(n in 1:n.search){
+    gamma.array[p,n] <- invlogit(gamma.combine$B0[p] + gamma.combine$B1[p]*site.max + gamma.combine$B2[p]*neighbors[n])
+  }
+}
+
+gamma.df <- adply(ph.array, c(1,2))
+colnames(gamma.df) <- c("p", "neighbor", "gamma")
+gamma.df$neighbor <- as.numeric(gamma.df$neighbor) - 1
+
+
+# ##### 3d heat map ####
+# # generate data, random + linear trend in x + linear trend in y
+# data = matrix(data = runif(n = 100, min = 0, max = 1), nrow=10, ncol = 10, dimnames=list(paste0('x',1:10),paste0('y',1:10)))
+# data = sweep(x = data, MARGIN = 1, 10:1, FUN = '+')
+# data = sweep(x = data, MARGIN = 2, 1:10, FUN = '+')
+# 
+# # generate 'empty' persp plot
+# pmat = persp(x=c(0,10), y=c(0,10), z=matrix(c(0,.1,0,.1), nrow=2), 
+#              xlim=c(0,10), ylim=c(0,10), zlim=c(0,20), 
+#              xlab='x', ylab='y', zlab='z', 
+#              theta=60, phi=20, d=2, box=F) 
+# 
+# # define color ramp
+# my_cols = heat.colors(10)
+# 
+# # generate color matrix (values between 1 and 10, corresponding to 10 values my_cols
+# colmat = matrix(data = 1, ncol = 10, nrow = 10)
+# colmat[1,1:10] <- 5
+# colmat[5,2:4] <- 8
+# colmat[6,8] <- 3
+# 
+# # draw each bar: from left to right ...
+# for (i in 1:nrow(data)){
+#   
+#   # ... and back to front 
+#   for (j in ncol(data):1){
+#     
+#     xy = which(data == data[i,j], arr.ind=TRUE)
+#     
+#     # side facing y
+#     x = rep(xy[1],4)
+#     y = c(xy[2]-1,xy[2],xy[2],xy[2]-1)
+#     z = c(0,0,data[i,j],data[i,j])
+#     polygon(trans3d(x, y, z, pmat), col=my_cols[colmat[i,j]], border=1)
+#     
+#     #  side facing x
+#     x = c(xy[1]-1,xy[1],xy[1],xy[1]-1)
+#     y = rep(xy[2]-1,4)
+#     z = c(0,0,data[i,j],data[i,j])
+#     polygon(trans3d(x, y, z, pmat), col=my_cols[colmat[i,j]], border=1)
+#     
+#     # top side
+#     x = c(xy[1]-1,xy[1],xy[1],xy[1]-1)
+#     y = c(xy[2]-1,xy[2]-1,xy[2],xy[2])
+#     z = rep(data[i,j],4)
+#     polygon(trans3d(x, y, z, pmat), col=my_cols[colmat[i,j]], border=1)
+#     
+#   }
+# }
+# 
+# # define axis ranges etc
+# x.axis <- 1:ncol(data) - 0.5
+# min.x <- 0
+# max.x <- 10
+# y.axis <- 1:nrow(data) - 0.5 
+# min.y <- 0
+# max.y <- 10
+# z.axis <- seq(0, 10, by=10)
+# min.z <- 0
+# max.z <- 10
+# 
+# # add some distance between tick labels and the axis
+# xoffset = 1
+# yoffset = 0.5
+# zoffset = 0.5
+# ticklength = 0.2
+# 
+# # x axis ticks
+# tick.start <- trans3d(x.axis, min.y, min.z, pmat)
+# tick.end <- trans3d(x.axis, (min.y - ticklength), min.z, pmat)
+# segments(tick.start$x, tick.start$y, tick.end$x, tick.end$y)
+# 
+# # y axis ticks
+# tick.start <- trans3d(max.x, y.axis, min.z, pmat)
+# tick.end <- trans3d(max.x + ticklength, y.axis, min.z, pmat)
+# segments(tick.start$x, tick.start$y, tick.end$x, tick.end$y)
+# 
+# # z axis ticks
+# tick.start <- trans3d(min.x, min.y, z.axis, pmat)
+# tick.end <- trans3d(min.x, (min.y - ticklength), z.axis, pmat)
+# segments(tick.start$x, tick.start$y, tick.end$x, tick.end$y)
+# 
+# # x labels
+# labels <- rownames(data)
+# label.pos <- trans3d(x.axis, (min.y - xoffset), min.z, pmat)
+# text(label.pos$x, label.pos$y, labels=labels, adj=c(0, NA), srt=0, cex=0.6)
+# 
+# # y labels
+# labels <- colnames(data)
+# label.pos <- trans3d((max.x + yoffset), y.axis, min.z, pmat)
+# text(label.pos$x, label.pos$y, labels=labels, adj=c(0, NA), srt=0, cex=0.6)
+# 
+# # z labels
+# labels <- as.character(z.axis)
+# label.pos <- trans3d(min.x, (min.y - zoffset), z.axis, pmat)
+# text(label.pos$x, label.pos$y, labels=labels, adj=c(1, NA), srt=0, cex=0.6) 
+# 
+# 
